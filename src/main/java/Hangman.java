0ec463312a6fd4bad54dc6d987143a0c37f0f334
Janet Isak <janetisak@yahoo.com>
2018-05-08 10:23:37 +0300

    creat the functions

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -2,32 +2,74 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Hangman {
-  Scanner scanner = new Scanner(System.in);
-  Random random = new Random();
-  String [] guesses = ("reddit", "programming", "fruit", "donut");
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      Random random = new Random();
+      String[] guesses = {"reddit", "programming", "fruit", "donut"};
 
 
-  boolean weArePlayng = true;
-  while (weArePlayng) {
-    System.out.printing("welcome to my game of Hangman"); //redddit  -> r,e,d,d,i,t
-    char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].toCharArray();
-    int amountOfGuesses = randomWordToGuess.length; //100
-    char[] playerGuess = new char[amountOfGuesses]; // ____
+      boolean weArePlayng = true;
+      while (weArePlayng) {
+          System.out.println("welcome to my game of Hangman"); //redddit  -> r,e,d,d,i,t
+          char[] randomWordToGuess = guesses[random.nextInt(guesses.length)].toCharArray();
+          int amountOfGuesses = randomWordToGuess.length; //100
+          char[] playerGuess = new char[amountOfGuesses]; // ____
 
 
-    for (int i = 0; i < playerGuess.length; i++) {
-        playerGuess[i] = '_';
-    }
+          for (int i = 0; i < playerGuess.length; i++) {
+              playerGuess[i] = '_';
+          }
 
 
-    boolean wordIsGuessed = false;
-    int tries = 0;
+          boolean wordIsGuessed = false;
+          int tries = 0;
 
-    while (!wordIsGuessed && tries != amountOfGuesses) {
-      
-    }
-  }
+          while (!wordIsGuessed && tries != amountOfGuesses) {
+              System.out.print("Current guesses:");
+              printArray(playerGuess);
+              System.out.printf("You have %d tries left.\n", amountOfGuesses - tries);
+              System.out.println("Enter a single character");
+              char input = scanner.nextLine().charAt(0);
+              tries++;
 
+              if (input == '-') {
+                  weArePlayng = false;
+                  wordIsGuessed = true;
+                } else {
+                    for (int i = 0; i < randomWordToGuess.length; i++) {
+                        if (randomWordToGuess[i] == input) {
+                            playerGuess[i] = input;
+                        }
+                    }
 
+                    if (isTheWordGuessed(playerGuess)) {
+                        wordIsGuessed = true;
+                        System.out.println("Congratulation you won!");
+                    }
+                }
+            }
+
+                if (!wordIsGuessed) System.out.println("You ran out of guesses :/");
+        }
+
+        System.out.println("Do you want to play another game? (yes/no)");
+        String anotherGame = scanner.nextLine();
+        if (anotherGame.equals("no")) weArePlayng = false;
+
+        System.out.println("Game over.");
 
 }
+
+public static void printArray(char[] array) {
+    for (int i = 0; i < array.length; i++) {
+        System.out.print(array[i] + "");
+    }
+    System.out.println();
+}
+
+public static boolean isTheWordGuessed(char[] array) {
+    for (int i = 0; i < array.length; i++) {
+        if (array[i] == '_') return false;
+    }
+    return true;
+}}
